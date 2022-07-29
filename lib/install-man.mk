5f836211c0370eeeafea48053f2f5f45a29ab3f3
Alejandro Colomar <alx.manpages@gmail.com>
2022-07-29 15:11:00 +0200

    Makefile, install-man.mk: Support man3head, and remove support for man0
    Signed-off-by: Alejandro Colomar <alx.manpages@gmail.com>

--- lib/install-man.mk
+++ lib/install-man.mk
@@ -12,22 +12,22 @@ include $(srcdir)/lib/install.mk
 
 
 mandir      := $(datarootdir)/man
-man0dir     := $(mandir)/man0
 man1dir     := $(mandir)/man1
 man2dir     := $(mandir)/man2
 man2typedir := $(mandir)/man2type
 man3dir     := $(mandir)/man3
+man3headdir := $(mandir)/man3head
 man3typedir := $(mandir)/man3type
 man4dir     := $(mandir)/man4
 man5dir     := $(mandir)/man5
 man6dir     := $(mandir)/man6
 man7dir     := $(mandir)/man7
 man8dir     := $(mandir)/man8
-man0ext     := .0
 man1ext     := .1
 man2ext     := .2
 man2typeext := .2type
 man3ext     := .3
+man3headext := .3head
 man3typeext := .3type
 man4ext     := .4
 man5ext     := .5
@@ -38,11 +38,11 @@ _mandir     := $(DESTDIR)$(mandir)
 
 
 _manpages      := $(patsubst $(MANDIR)/%,$(_mandir)/%,$(MANPAGES))
-_man0pages     := $(filter %$(man0ext),$(_manpages))
 _man1pages     := $(filter %$(man1ext),$(_manpages))
 _man2pages     := $(filter %$(man2ext),$(_manpages))
 _man2typepages := $(filter %$(man2typeext),$(_manpages))
 _man3pages     := $(filter %$(man3ext),$(_manpages))
+_man3headpages := $(filter %$(man3headext),$(_manpages))
 _man3typepages := $(filter %$(man3typeext),$(_manpages))
 _man4pages     := $(filter %$(man4ext),$(_manpages))
 _man5pages     := $(filter %$(man5ext),$(_manpages))
@@ -51,11 +51,11 @@ _man7pages     := $(filter %$(man7ext),$(_manpages))
 _man8pages     := $(filter %$(man8ext),$(_manpages))
 
 _mandirs     := $(patsubst $(MANDIR)/%,$(_mandir)/%/.,$(MANDIRS))
-_man0dir     := $(filter %man0/.,$(_mandirs))
 _man1dir     := $(filter %man1/.,$(_mandirs))
 _man2dir     := $(filter %man2/.,$(_mandirs))
 _man2typedir := $(filter %man2type/.,$(_mandirs))
 _man3dir     := $(filter %man3/.,$(_mandirs))
+_man3headdir := $(filter %man3head/.,$(_mandirs))
 _man3typedir := $(filter %man3type/.,$(_mandirs))
 _man4dir     := $(filter %man4/.,$(_mandirs))
 _man5dir     := $(filter %man5/.,$(_mandirs))
@@ -63,11 +63,11 @@ _man6dir     := $(filter %man6/.,$(_mandirs))
 _man7dir     := $(filter %man7/.,$(_mandirs))
 _man8dir     := $(filter %man8/.,$(_mandirs))
 
-_man0pages_rm     := $(addsuffix -rm,$(wildcard $(_man0pages)))
 _man1pages_rm     := $(addsuffix -rm,$(wildcard $(_man1pages)))
 _man2pages_rm     := $(addsuffix -rm,$(wildcard $(_man2pages)))
 _man2typepages_rm := $(addsuffix -rm,$(wildcard $(_man2typepages)))
 _man3pages_rm     := $(addsuffix -rm,$(wildcard $(_man3pages)))
+_man3headpages_rm := $(addsuffix -rm,$(wildcard $(_man3headpages)))
 _man3typepages_rm := $(addsuffix -rm,$(wildcard $(_man3typepages)))
 _man4pages_rm     := $(addsuffix -rm,$(wildcard $(_man4pages)))
 _man5pages_rm     := $(addsuffix -rm,$(wildcard $(_man5pages)))
@@ -76,11 +76,11 @@ _man7pages_rm     := $(addsuffix -rm,$(wildcard $(_man7pages)))
 _man8pages_rm     := $(addsuffix -rm,$(wildcard $(_man8pages)))
 
 _mandirs_rmdir     := $(addsuffix -rmdir,$(wildcard $(_mandirs)))
-_man0dir_rmdir     := $(addsuffix -rmdir,$(wildcard $(_man0dir)))
 _man1dir_rmdir     := $(addsuffix -rmdir,$(wildcard $(_man1dir)))
 _man2dir_rmdir     := $(addsuffix -rmdir,$(wildcard $(_man2dir)))
 _man2typedir_rmdir := $(addsuffix -rmdir,$(wildcard $(_man2typedir)))
 _man3dir_rmdir     := $(addsuffix -rmdir,$(wildcard $(_man3dir)))
+_man3headdir_rmdir := $(addsuffix -rmdir,$(wildcard $(_man3headdir)))
 _man3typedir_rmdir := $(addsuffix -rmdir,$(wildcard $(_man3typedir)))
 _man4dir_rmdir     := $(addsuffix -rmdir,$(wildcard $(_man4dir)))
 _man5dir_rmdir     := $(addsuffix -rmdir,$(wildcard $(_man5dir)))
@@ -89,7 +89,7 @@ _man7dir_rmdir     := $(addsuffix -rmdir,$(wildcard $(_man7dir)))
 _man8dir_rmdir     := $(addsuffix -rmdir,$(wildcard $(_man8dir)))
 _mandir_rmdir      := $(addsuffix -rmdir,$(wildcard $(_mandir)/.))
 
-MAN_SECTIONS     := 0 1 2 2type 3 3type 4 5 6 7 8
+MAN_SECTIONS     := 1 2 2type 3 3head 3type 4 5 6 7 8
 install_manX     := $(foreach x,$(MAN_SECTIONS),install-man$(x))
 installdirs_manX := $(foreach x,$(MAN_SECTIONS),installdirs-man$(x))
 uninstall_manX   := $(foreach x,$(MAN_SECTIONS),uninstall-man$(x))
